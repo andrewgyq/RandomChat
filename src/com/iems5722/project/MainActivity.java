4dd4282625273805275e7acc2eba1458ee4e555d
andrewgyq <514362283@qq.com>
2015-03-04 23:19:34 +0800

    2015-03-04 andrewgyq

--- src/com/iems5722/project/MainActivity.java
+++ src/com/iems5722/project/MainActivity.java
@@ -1,34 +1,84 @@
 package com.iems5722.project;
 
+import java.io.IOException;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.ParseException;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
-
+	
+	private Button button;
+	private TextView textView;
+	private String serverUrl = "http://54.169.108.112:3000/";
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		button = (Button) findViewById(R.id.button);
+		
+		button.setOnClickListener(new View.OnClickListener() {
+		    @Override
+		    public void onClick(View v) {
+		    	findMatchingUser();
+		    }
+
+		});
+	}
+	
+	
+	protected void findMatchingUser() {
+		new AsyncTask< Void, Void, String>(){
+
+			@Override
+			protected String doInBackground(Void... params) {
+				HttpClient client = new DefaultHttpClient();
+				
+				HttpGet request = new HttpGet(serverUrl);
+				HttpResponse response = null;
+				try {
+					response = client.execute(request);
+				} catch (ClientProtocolException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				
+				HttpEntity entity = response.getEntity();
+				
+				String targetUrl = null;
+				try {
+					targetUrl = EntityUtils.toString(entity);
+				} catch (ParseException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				return targetUrl;
+			}
+			
+			@Override
+		    protected void onPostExecute( String targetUrl) {
+		        super.onPostExecute(targetUrl);
+		        textView = (TextView) findViewById(R.id.targetUrl);
+		        textView.setText(targetUrl);
+		    }
+			
+		}.execute(null, null, null);
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
 }
